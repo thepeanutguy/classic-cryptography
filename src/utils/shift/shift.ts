19d593d96475f8058a6ae2bf1bc7eeb4006c7514
Kieran Parker <kieranmparker@protonmail.com>
2025-12-22 12:06:57 +0000

    package.json updates

--- src/utils/shift/shift.ts
+++ src/utils/shift/shift.ts
@@ -1,11 +1,11 @@
-import mod from '#/utils/mod';
-import type { ShiftFn } from './shift.interface';
+import mod from "#/utils/mod";
+import type { ShiftFn } from "./shift.interface";
 
 const CHAR_A = 65;
 const CHAR_Z = 90;
 
 const shift: ShiftFn = (plainText, key) => {
-  let cipherText = '';
+  let cipherText = "";
 
   for (let i = 0; i < plainText.length; i += 1) {
     const letter = plainText[i];
